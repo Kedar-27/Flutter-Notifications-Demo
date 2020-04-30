2c2c17ad42f255b60c48a859ab67826d35cbef2b
Kedar Sukerkar <kedarjsukerkar@yahoo.co.in>
2020-04-30 16:01:32 +0530

    Local notifications(iOS) fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as statusCodes;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-const URL = 'ws://YOUR.SERVER.URL:PORT';
+const URL = 'ws://139.59.50.81:3000';
 
 
 void main() =>
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
 class FauxLoginPage extends StatelessWidget {
   final TextEditingController controller = TextEditingController();
 
+
+
+
   void goToMainPage(String nickname, BuildContext context) {
     Navigator.push(
         context,
@@ -30,6 +34,8 @@ class FauxLoginPage extends StatelessWidget {
         )
     );
   }
+
+
 
   @override
   Widget build(BuildContext context) =>
@@ -71,13 +77,17 @@ class AnnouncementPageState extends State<AnnouncementPage> {
   String text;
 
   @override
-  void initState() {
+   void initState() {
     super.initState();
+
+    //createLocalNotification();
 
     FlutterLocalNotificationsPlugin notifications = FlutterLocalNotificationsPlugin();
     var androidInit = AndroidInitializationSettings('app_icon');
-    var iOSInit = IOSInitializationSettings();
+    var iOSInit = IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
     var init = InitializationSettings(androidInit, iOSInit);
+
+
     notifications.initialize(init).then((done) {
       sub = channel.stream.listen((newData) {
         setState(() {
@@ -100,6 +110,53 @@ class AnnouncementPageState extends State<AnnouncementPage> {
       });
     });
   }
+
+  /// For manual displaying local notification
+  Future createLocalNotification() async{
+
+    FlutterLocalNotificationsPlugin notifications = FlutterLocalNotificationsPlugin();
+    var androidInit = AndroidInitializationSettings('app_icon');
+    var iOSInit = IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var init = InitializationSettings(androidInit, iOSInit);
+
+
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await notifications.show(
+        0, 'Test title', 'Test body', platformChannelSpecifics,
+        payload: 'item x');
+
+
+  }
+
+
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: Text('Ok'),
+            onPressed: () async {
+              Navigator.of(context, rootNavigator: true).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
 
   @override
   void dispose() {
